18a45bcb1ed3f9a2b776ecd7366abd572a769905
DESKTOP-BAI437V\yaobo <yaobozhang@163.com>
2020-02-14 16:27:11 +0800

    no message

--- 会员体系/精准营销/电商v2.0/疾病_活动/脚本/高端会员答谢会活动脚本.sql
+++ 会员体系/精准营销/电商v2.0/疾病_活动/脚本/高端会员答谢会活动脚本.sql
@@ -1,85 +1,44 @@
---主题：流失高等级会员答谢会
+--主题：高等级会员答谢会
 --BY：大数据管理部 姚泊彰
---DATE: 20191213
+--DATE: 20200212
 
---STEP1：得到目标会员并落表
---会员标签时间20191211
-drop table "EXT_TMP"."YBZ_MEMB_INFO_20191213";
-create column table "EXT_TMP"."YBZ_MEMB_INFO_20191213" as
-(
-        select member_id				--会员ID
-				,MEMB_LIFE_CYCLE		--生命周期
-				,case when MAIN_CNSM_DEPRT_CODE='10011441' then 'changsha'													--分析地点为长沙和宁乡
-				else 'ningxiang' end as memb_place		--会员主消费地方
-				,OFFLINE_LAST_CNSM_DATE	--最后一次消费时间
-				,MAIN_CNSM_PHMC_CODE	--主消费门店
-		from "_SYS_BIC"."YF_BI.EXT_APP.OTHERS/CV_MEMB_CNT_INFO" t1
-		WHERE OFFLINE_LAST_CNSM_DATE>add_months('20191211',-18)		--最后一次消费时间是180天以内
-		and MEMB_LIFE_CYCLE in ('06','07')																					--选择生命周期
-		and (																												--选择活动地点
-				MAIN_CNSM_DEPRT_CODE ='10011441' 	--长沙门管部
-				OR 
-				MAIN_CNSM_DIST_CODE IN ('10012924','10017063') --宁乡一区和宁乡二区
-			)
-		and exists(																											--选择消费条件
-			select 1 from (
-				select MEMBER_ID
-					,sum(SALE_AMT) as SALE_AMT
-				from "_SYS_BIC"."YF_BI.DW.CRM/CV_REEF_SALE_ORDER_DETL"('PLACEHOLDER' = ('$$EndTime$$',
-					'20191211'),	--会员一年半内消费
-					'PLACEHOLDER' = ('$$BeginTime$$',
-					'20180611'))
-				group by MEMBER_ID
-			)t2 
-			where t1.member_id=t2.member_id
-			and SALE_AMT >= 400
+--STEP1：选择目标区域，特定条件下的会员，得到会员的生命周期、主消费区域、最后一次消费时间等信息
+with t1 as (
+	select member_id				--会员ID
+		,MEMB_LIFE_CYCLE		--生命周期
+		--,'Act_Place' as memb_place		--分析地点标识
+		,OFFLINE_LAST_CNSM_DATE	--最后一次消费时间
+		,MAIN_CNSM_PHMC_CODE	--主消费门店
+	from "_SYS_BIC"."YF_BI.EXT_APP.OTHERS/CV_MEMB_CNT_INFO" t1 				--ps:这个视图选的是七天内最新的一天
+	WHERE OFFLINE_LAST_CNSM_DATE>add_months('20191211',-18)		--最后一次消费时间是18个月以内的
+	and MEMB_LIFE_CYCLE in ('06','07')																					--选择生命周期
+	and (																												--选择活动地点
+			MAIN_CNSM_DEPRT_CODE ='10011441' 	--长沙门管部
+			OR 
+			MAIN_CNSM_DIST_CODE IN ('10012924','10017063') --宁乡一区和宁乡二区
 		)
-);
-commit;
+	and exists(																											--选择消费条件
+		select 1 from (
+			select MEMBER_ID
+				,sum(SALE_AMT) as SALE_AMT
+			from "_SYS_BIC"."YF_BI.DW.CRM/CV_REEF_SALE_ORDER_DETL"('PLACEHOLDER' = ('$$EndTime$$',
+				'20191211'),	--会员一年半内消费
+				'PLACEHOLDER' = ('$$BeginTime$$',
+				'20180611'))
+			group by MEMBER_ID
+		)t2 
+		where t1.member_id=t2.member_id
+		and SALE_AMT >= 400
+	)
+)
 
---STEP2:得到会员推荐或消费商品信息
---把会员池进行封装，可以统一修改源
-with t1_1 as (
-	select member_id
-		,memb_life_cycle
-		,memb_place
-		,OFFLINE_LAST_CNSM_DATE			
-		,MAIN_CNSM_PHMC_CODE
-	from "EXT_TMP"."YBZ_MEMB_INFO_20191213"
-	where memb_place='changsha'								--选择哪个地方的人
-)
-,
---得到不同地点的商品池数据
-t1_2 as (
-	
-	SELECT t1.DESEASE_LEV2_CODE		--疾病二级
-		,t1.GOODS_CODE				--关联商品
-		,t1.GOODS_NAME				
-		,case when t2.GOODS_CODE is not null then 1 else 0 end as changsha_flag		--是否长沙商品池
-		,case when t2.GOODS_CODE is not null then 1 else 0 end as ningxiang_flag	--是否宁乡商品池
-	FROM 
-	(
-	SELECT DESEASE_LEV2_CODE,GOODS_CODE,max(GOODS_NAME) as GOODS_NAME		--首先，去重，得到疾病关联商品去重后数据
-	FROM "EXT_TMP"."BOZHANG_DISEASE_UNION_GOOD"
-	GROUP BY DESEASE_LEV2_CODE,GOODS_CODE
-	) t1
-	left join "EXT_TMP"."BOZHANG_CHANGSHA_GOODS" t2		--关联长沙商品池
-	on  T1.GOODS_CODE=T2.GOODS_CODE
-	left join "EXT_TMP"."BOZHANG_NINGXIANG_GOODS" t3		--关联宁乡商品池
-	on T1.GOODS_CODE=T3.GOODS_CODE
-)
-,
-t1_3 as (
-	SELECT DESEASE_LEV2_CODE
-		,GOODS_CODE
-		,GOODS_NAME
-	FROM T1_2
-	WHERE changsha_flag=1								--选择哪个地方的商品池
-
-)
-,
---得到商品消费数据，按消费人数排序
-t1 as (
+--STEP2：得到这些会员消费商品数据，并得到消费人数前N（N=300）商品，给出人均购买数量及其排名；
+--这些商品给到业务方，业务方确认后得到本次活动商品池，导入到系统tmp表中,表名："EXT_TMP"."BOZHANG_Act_Place_GOODS"
+--业务完善疾病相关用药数据，确保本次活动商品都有疾病相关用药记录，导入到系统tmp表中 "EXT_TMP"."BOZHANG_DISEASE_UNION_GOOD"
+--使用疾病相关用药数据关联本次活动商品池数据，得到疾病可推本次活动商品池数据
+/*,
+--STEP2.1：得到活动区域会员详细消费数据，需要改消费时间
+t2_1 as (
 		SELECT
 			 t."STSC_DATE",  								--销售日期
 			 t."PHMC_CODE",     							--门店编码
@@ -97,9 +56,9 @@ t1 as (
 			,t1.OFFLINE_LAST_CNSM_DATE			
 			,t1.MAIN_CNSM_PHMC_CODE
 		FROM "_SYS_BIC"."YF_BI.DW.CRM/CV_REEF_SALE_ORDER_DETL"('PLACEHOLDER' = ('$$EndTime$$',
-			 '20191211'),				--当前会员标签
+			 '20191211'),																	--改消费时间
 			 'PLACEHOLDER' = ('$$BeginTime$$',
-			 '20180611')) t 			--往前一年半
+			 '20180611')) t 																--往前一年半
 		inner join t1_1 t1
 		on t.MEMBER_ID=t1.MEMBER_ID
 		left join dw.DIM_GOODS_H g on g.goods_sid=t.goods_sid		--使用left join，防止商品不存在
@@ -118,14 +77,20 @@ t1 as (
 )
 
 ,
+--STEP2.2：得到消费人数前N（N=300）商品
 --看每个地方销售人数排名前300商品
-t2 as (
-	select memb_place,GOODS_CODE,GOODS_NAME,memb_num
-		,row_number() over(partition by memb_place order by memb_num desc) as rn
+t2_2 as (
+	select memb_place,GOODS_CODE,GOODS_NAME,memb_num,memb_avg_num
+		,row_number() over(partition by memb_place order by memb_num desc) as memb_num_rn
+		,row_number() over(partition by memb_place order by memb_avg_num desc) as memb_avg_num_rn
 	from
 	(
-		select memb_place,GOODS_CODE,GOODS_NAME,count(distinct MEMBER_ID) as memb_num
-		from t1
+		select memb_place
+			,GOODS_CODE
+			,GOODS_NAME
+			,count(distinct MEMBER_ID) as memb_num
+			,sum(SALE_QTY)/count(distinct MEMBER_ID) as memb_avg_num
+		from t2_1
 		where GOODS_NAME is not null
 		AND PROD_CATE_LEV1_CODE!='Y11'	--不要个人护理
 		AND  PROD_CATE_LEV1_CODE!='Y13'		--不要日常用品
@@ -134,27 +99,36 @@ t2 as (
 	)
 
 )
+--给出数据到业务方
+select * from t2_2 where memb_num_rn<=300
+*/
 ,
---看每个地方人均购买数量排名前300商品
-t3 as (
-	select memb_place,GOODS_CODE,GOODS_NAME,memb_avg_num,memb_num
-		,row_number() over(partition by memb_place order by memb_avg_num desc) as rn
-	from
+--STEP2.3：使用疾病相关用药数据关联本次活动商品池数据，得到疾病可推本次活动商品池数据
+t2_3 as (
+	
+	SELECT t1.DESEASE_LEV2_CODE		--疾病二级
+		,t1.GOODS_CODE				--关联商品编码
+		,t1.GOODS_NAME				--关联商品名称
+	FROM 
 	(
-		select memb_place,GOODS_CODE,GOODS_NAME,sum(SALE_QTY)/count(distinct MEMBER_ID) as memb_avg_num,count(distinct MEMBER_ID) as memb_num
-		from t1
-		where GOODS_NAME is not null
-		AND PROD_CATE_LEV1_CODE!='Y11'
-		AND  PROD_CATE_LEV1_CODE!='Y13'
-		AND PROD_CATE_LEV4_NAME!='散装类'
-		group by memb_place,GOODS_CODE,GOODS_NAME
+		SELECT DESEASE_LEV2_CODE,GOODS_CODE,max(GOODS_NAME) as GOODS_NAME		--首先，去重，得到疾病关联商品去重后数据
+		FROM "EXT_TMP"."BOZHANG_DISEASE_UNION_GOOD"
+		GROUP BY DESEASE_LEV2_CODE,GOODS_CODE
+	) t1
+	inner join 
+	(	--选择活动区域的商品
+		select GOODS_CODE,Act_Place 
+		from "EXT_TMP"."BOZHANG_CHANGSHA_GOODS"
+		where Act_Place='changsha'											--此处需要修改为活动区域
 	)
-
+	t2		--关联活动商品池
+	on  T1.GOODS_CODE=T2.GOODS_CODE
 )
+
+--STEP3：根据STEP1中的会员数据得到这些会员的主疾病（疾病消费天数最多的疾病）
+--STEP3.1：首先，同人同天算一次
 ,
---STEP4:得到会员疾病标签
---首先，同人同天算一次
-t4_1 as (
+t3_1 as (
 	SELECT STSC_DATE,
 		GOODS_CODE,
 		MEMBER_ID,
@@ -167,21 +141,24 @@ t4_1 as (
 		MEMBER_ID
 	
 ) 
---得到疾病-药数据
---首先得到主治用药，并得到每个药的条数
+--STEP3.2：得到疾病-药数据
+--取出疾病表中只有一条主治用药的单品
 ,
-t4_2 as (
-	select GOODS_CODE,count(1) as num from "DS_COL"."SUCCEZ_GOODS_DISEASE" where EFFECT_SCORE=2 group by GOODS_CODE
-)
---然后，取出疾病表中只有一条主治用药的单品
-,
-t4_3 as (
+t3_2 as (
 	select t0.GOODS_CODE,t1.DISEASE_LV1_NAME DISEASE_NAME_LEV1
 		,t1.DISEASE_LV2_NAME DISEASE_NAME_LEV2 
 		,t1.DISEASE_LV2_CODE DISEASE_CODE_LEV2
 	from
 	(
-		select GOODS_CODE from t4_2 where num=1
+		select GOODS_CODE 
+		from (
+			select GOODS_CODE
+				,count(1) as num 
+			from "DS_COL"."SUCCEZ_GOODS_DISEASE" 
+			where EFFECT_SCORE=2 
+			group by GOODS_CODE
+		)
+		where num=1
 	)t0
 	left join 
 	(
@@ -190,29 +167,27 @@ t4_3 as (
 	t1 
 	on t0.GOODS_CODE=t1.GOODS_CODE
 ) 
-,
---关联得到每个会员每天买过的疾病情况，并过滤掉疾病为空的天数
-t4_4 as (
-	select  t1.MEMBER_ID,						--会员编码
-		 t1.STSC_DATE,  					--销售日期
-		 t1.GOODS_CODE,    				--商品编码
-		 t1.memb_place,
-		 t2.DISEASE_NAME_LEV1,			--疾病一级
-		 t2.DISEASE_NAME_LEV2			--疾病二级
-		,t2.DISEASE_CODE_LEV2			--疾病二级	
-	from t4_1 t1 
-	left join t4_3 t2
-	on t1.GOODS_CODE=t2.GOODS_CODE
 
-)
+--STEP3.3：关联得到每个会员每天买过的疾病情况，并过滤掉疾病为空的天数
 ,
-t4_5 as (
+t3_3 as (
 	select MEMBER_ID
 		,STSC_DATE
 		,DISEASE_CODE_LEV2
 		,DISEASE_NAME_LEV2
 		,max(memb_place) as memb_place
-	FROM t4_4
+	FROM (
+		select  t1.MEMBER_ID						--会员编码
+			,t1.STSC_DATE  					--销售日期
+			,t1.GOODS_CODE    				--商品编码
+			,t1.memb_place
+			,t2.DISEASE_NAME_LEV1			--疾病一级
+			,t2.DISEASE_NAME_LEV2			--疾病二级
+			,t2.DISEASE_CODE_LEV2			--疾病二级	
+		from t3_1 t1 
+		left join t3_2 t2
+		on t1.GOODS_CODE=t2.GOODS_CODE
+	)
 	where DISEASE_NAME_LEV2 is not null
 	group by MEMBER_ID
 		,STSC_DATE
@@ -220,9 +195,9 @@ t4_5 as (
 		,DISEASE_NAME_LEV2
 )
 
---step4:对每个会员数据取疾病购买天数最多疾病
+--对每个会员数据取疾病购买天数最多疾病
 ,
-t4 as (
+t3 as (
 	select member_id,memb_place,DISEASE_CODE_LEV2,DISEASE_NAME_LEV2,rn
 	from
 	(
@@ -230,12 +205,12 @@ t4 as (
 		from
 		(
 			select distinct member_id
-			from t4_4
+			from t3_1
 		)t1
 		left join 
 		(
 			select member_id,max(memb_place) as memb_place,DISEASE_CODE_LEV2,DISEASE_NAME_LEV2,count(1) as day_num 
-			from t4_5
+			from t3_3
 			group by member_id,DISEASE_CODE_LEV2,DISEASE_NAME_LEV2
 		)t2
 		ON T1.member_id=T2.member_id
@@ -243,14 +218,16 @@ t4 as (
 	)
 	where rn=1
 )
+
+--STEP4：根据STEP2和STEP3得到这些会员主疾病关联商品，对每个会员关联到的商品选择买过的，作为主推荐池
 ,
---得到会员疾病关联商品，主要选买过的
-t5_1 as (
+t4 as (
 	SELECT T1.member_id
 		,T1.DISEASE_CODE_LEV2
 		,T1.DISEASE_NAME_LEV2
 		,T1.GOODS_CODE
 		,T1.GOODS_NAME
+		,5 AS GOODS_LEVEL					--优先级为5
 		,CASE WHEN T2.GOODS_CODE IS NOT NULL THEN 1 ELSE 0 END AS IF_BUF_FLAG
 		,T2.LAST_BUY_DATE		--最后一次购买时间
 		,T2.MAX_PRICE			--最高成交价
@@ -263,8 +240,8 @@ t5_1 as (
 			,T1.DISEASE_NAME_LEV2
 			,T2.GOODS_CODE			--会员疾病关联商品
 			,T2.GOODS_NAME
-		FROM T4 T1
-		LEFT JOIN t1_3 T2
+		FROM T3 T1
+		LEFT JOIN t2_3 T2
 		ON T1.DISEASE_CODE_LEV2=T2.DESEASE_LEV2_CODE
 	)T1
 	LEFT JOIN 
@@ -281,37 +258,77 @@ t5_1 as (
 	AND T1.GOODS_CODE=T2.GOODS_CODE
 )
 ,
---找到每个会员通过疾病可以推荐的商品
-T5 AS (
-	select T1.member_id
-		,T1.DISEASE_CODE_LEV2
-		,T1.DISEASE_NAME_LEV2
-		,T1.GOODS_CODE
-		,T1.GOODS_NAME
-		,t1.IF_BUF_FLAG
-		,T1.LAST_BUY_DATE		--最后一次购买时间
-		,T1.MAX_PRICE			--最高成交价
-		,T1.MIN_PRICE			--最低成交价
-	from
-	(
-		SELECT T1.member_id
-			,T1.DISEASE_CODE_LEV2
-			,T1.DISEASE_NAME_LEV2
-			,T1.GOODS_CODE
-			,T1.GOODS_NAME
-			,t1.IF_BUF_FLAG
-			,T1.LAST_BUY_DATE		--最后一次购买时间
-			,T1.MAX_PRICE			--最高成交价
-			,T1.MIN_PRICE			--最低成交价
-			,row_number() over(partition by member_id order by IF_BUF_FLAG desc,LAST_BUY_DATE desc,GOODS_CODE asc) as rn
-		FROM T5_1 t1
-	)t1
-	where rn=1
+
+--STEP5：根据STEP1和STEP2得到这些会员买过的本次活动商品数据，作为补充池
+t5 as (
+	SELECT MEMBER_ID
+		,null as DISEASE_CODE_LEV2
+		,null as DISEASE_NAME_LEV2
+		,GOODS_CODE
+		,max(GOODS_NAME) as GOODS_NAME
+		,3 AS GOODS_LEVEL						--优先级为5
+		,MAX(STSC_DATE) AS LAST_BUY_DATE		--最后一次购买时间
+		,sum(SALE_QTY) as SALE_QTY		--销售数量
+		,MAX(ACNT_PRIC) AS MAX_PRICE			--最高成交价
+		,MIN(ACNT_PRIC) AS MIN_PRICE			--最低成交价
+	FROM T1
+	where exists(
+		select 1 from
+		t2_3 t2
+		where t1.goods_code=t2.goods_code
+	)
+	group by member_id,goods_code
 )
 ,
 
---得到每个会员基本信息
+--STEP6：把STEP4和STEP5中的数据合并，定义数据优先级为购买过的主疾病关联商品(按最近购买时间定优先级)>购买过的本次活动商品（按最近购买时间定优先级）
 t6 as (
+	select member_id
+		,DISEASE_CODE_LEV2
+		,DISEASE_NAME_LEV2
+		,GOODS_CODE
+		,GOODS_NAME
+		,GOODS_LEVEL					--优先级为5
+		,IF_BUF_FLAG		--是否购买
+		,SALE_QTY	--购买数量
+		,LAST_BUY_DATE		--最后一次购买时间
+		,MAX_PRICE			--最高成交价
+		,MIN_PRICE			--最低成交价
+		,row_number() over(partition by member_id order by GOODS_LEVEL desc,IF_BUF_FLAG desc,LAST_BUY_DATE desc,SALE_QTY DESC) as rn
+	from
+	(
+		select member_id
+			,DISEASE_CODE_LEV2
+			,DISEASE_NAME_LEV2
+			,GOODS_CODE
+			,GOODS_NAME
+			,GOODS_LEVEL					--优先级为5
+			,IF_BUF_FLAG		--是否购买
+			,NULL AS SALE_QTY	--购买数量
+			,LAST_BUY_DATE		--最后一次购买时间
+			,MAX_PRICE			--最高成交价
+			,MIN_PRICE			--最低成交价
+		from t4
+		union all
+		select member_id
+			,DISEASE_CODE_LEV2
+			,DISEASE_NAME_LEV2
+			,GOODS_CODE
+			,GOODS_NAME
+			,GOODS_LEVEL					--优先级为3
+			,IF_BUF_FLAG		--是否购买
+			,SALE_QTY
+			,LAST_BUY_DATE		--最后一次购买时间
+			,MAX_PRICE			--最高成交价
+			,MIN_PRICE			--最低成交价
+		from t5
+	)
+	
+)
+,
+
+--STEP7：每个会员选择一个优先级最高的商品，关联得到会员信息及会员购买商品相关信息得到最终结果
+t7 as (
 	select T1.member_id
 		,T1.DISEASE_CODE_LEV2
 		,T1.DISEASE_NAME_LEV2
@@ -332,7 +349,7 @@ t6 as (
 		,t4.sale_amt
 		,t4.sale_times
 		,T2.MEMB_CARD_CODE
-	from t5 T1
+	from t6 T1
 	left join DW.FACT_MEMBER_BASE  t2 
 	on t1.member_id=t2.MEMB_CODE
 	left join t1_1 t3
@@ -345,69 +362,13 @@ t6 as (
 		group by member_id
 	)t4
 	on t1.member_id=t4.member_id
+	WHERE T1.rn=1
 )
-,
---
-t7_1 as (
-	select MEMBER_ID
-			,GOODS_CODE
-			,GOODS_NAME
-			,LAST_BUY_DATE		--最后一次购买时间
-			,MAX_PRICE			--最高成交价
-			,MIN_PRICE			--最低成交价
-		from
-		(
-			select MEMBER_ID
-				,GOODS_CODE
-				,GOODS_NAME
-				,LAST_BUY_DATE		--最后一次购买时间
-				,MAX_PRICE			--最高成交价
-				,MIN_PRICE			--最低成交价
-				,row_number() over(partition by member_id order by SALE_QTY desc) as rn
-			from
-			(
-				SELECT MEMBER_ID
-					,GOODS_CODE
-					,max(GOODS_NAME) as GOODS_NAME
-					,MAX(STSC_DATE) AS LAST_BUY_DATE		--最后一次购买时间
-					,sum(SALE_QTY) as SALE_QTY		--销售数量
-					,MAX(ACNT_PRIC) AS MAX_PRICE			--最高成交价
-					,MIN(ACNT_PRIC) AS MIN_PRICE			--最低成交价
-				FROM T1
-				where exists(
-					select 1 from
-					t1_3 t2
-					where t1.goods_code=t2.goods_code
-				)
-				group by member_id,goods_code
-			)
-		)where rn=1
-)
-,
---如果没匹配到药品，用每个人活动商品中买得最多的作为补充
-t7 as (
-	select T1.member_id
-		,T1.DISEASE_CODE_LEV2
-		,T1.DISEASE_NAME_LEV2
-		,case when T1.GOODS_CODE is not null then T1.GOODS_CODE else t2.GOODS_CODE end as GOODS_CODE
-		,case when T1.GOODS_CODE is not null then T1.GOODS_NAME else t2.GOODS_NAME end as GOODS_NAME
-		,t1.IF_BUF_FLAG
-		,case when T1.GOODS_CODE is not null then T1.LAST_BUY_DATE else T2.LAST_BUY_DATE end as LAST_BUY_DATE		--最后一次购买时间
-		,case when T1.GOODS_CODE is not null then T1.MAX_PRICE else T2.MAX_PRICE end as MAX_PRICE 			--最高成交价
-		,case when T1.GOODS_CODE is not null then T1.MIN_PRICE else T2.MIN_PRICE end as MIN_PRICE 			--最低成交价
-		,T1.MEMB_NAME			--姓名
-		,T1.MEMB_GNDR			--性别
-		,t1.AGE 	--年龄
-		,T1.MEMB_MOBI				--手机号
-		,T1.BELONG_PHMC_CODE		--所属门店
-		,t1.memb_place				--主消费区域
-		,t1.OFFLINE_LAST_CNSM_DATE			--最后一次消费时间
-		,t1.MAIN_CNSM_PHMC_CODE			--主消费门店
-		,t1.sale_amt
-		,t1.sale_times
-		,T1.MEMB_CARD_CODE
-	from t6 t1
-	left join t7_1 t2
-	on t1.member_id=t2.member_id
-)
+
 select * from t7
+
+
+
+
+
+
